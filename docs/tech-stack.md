3e0fd4abe21802d3b0c35d594866d58fdf482a3e
jerryhuangyu <jerry0932619628@gmail.com>
2025-10-24 13:08:56 +0800

    feat: add CLI commands skelton for git profile management

---
+++ docs/tech-stack.md
@@ -0,0 +1,16 @@
+# Tech Stack
+
+## make-dir
+https://github.com/sindresorhus/make-dir?tab=readme-ov-file
+
+## simple-git
+https://github.com/steveukx/git-js#readme
+
+## listr
+https://github.com/SamVerschueren/listr
+
+## execa
+https://github.com/sindresorhus/execa
+
+## commander
+https://github.com/tj/commander.js?tab=readme-ov-file
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,63 @@
 #!/usr/bin/env node
 
-export function fn() {
-  return 'Git Me!'
-}
+import { Command } from "commander";
 
-console.log(fn());
+const program = new Command();
+
+program
+	.name("gitface")
+	.description("A simple CLI tool to change your “face” in Git")
+	.version("0.0.1");
+
+program
+	.command("new")
+	.description("Create a new git profile")
+	.argument("<name>", "name of the git profile")
+	.option("-f, --force", "overwrite if profile already exists")
+	.action((name, options) => {
+		const force = options.force || false;
+		if (force) {
+			console.log(`Forcing creation of profile: ${name}`);
+		}
+		console.log(`Creating profile: ${name}`);
+	});
+
+program
+	.command("ls")
+	.description("List all git profiles")
+	.action(() => {
+		console.log("Listing profiles...");
+	});
+
+program
+	.command("current")
+	.description("Show the current git profile")
+	.action(() => {
+		console.log("Showing current profile...");
+	});
+
+program
+	.command("edit")
+	.description("Edit a git profile")
+	.argument("<name>", "name of the git profile")
+	.action((name) => {
+		console.log(`Editing profile: ${name}`);
+	});
+
+program
+	.command("rm")
+	.description("Remove a git profile")
+	.argument("<name>", "name of the git profile")
+	.action((name) => {
+		console.log(`Removing profile: ${name}`);
+	});
+
+program
+	.command("use")
+	.description("Switch to a git profile")
+	.argument("<name>", "name of the git profile")
+	.action((name) => {
+		console.log(`Switching to profile: ${name}`);
+	});
+
+program.parse();
